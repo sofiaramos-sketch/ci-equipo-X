7d79dcf113b8f4a9fdd8583e4df1de1f627e4ba0
Damian <damian.marengo@cablasociados.com>
2026-04-29 18:40:35 -0300

    Tests para push

---
+++ tests/tests_MenuPrincipal.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+import menu_principal
+
+
+# Testea que mostrar_menu devuelve lo ingresado
+def test_mostrar_menu():
+    with patch("builtins.input", return_value="1"):
+        opcion = menu_principal.mostrar_menu()
+        assert opcion == "1"
+
+
+# Testea salir del programa
+def test_main_salir(capsys):
+    with patch("builtins.input", side_effect=["5"]):
+        menu_principal.main()
+
+    captured = capsys.readouterr()
+    assert "Saliendo del programa..." in captured.out
