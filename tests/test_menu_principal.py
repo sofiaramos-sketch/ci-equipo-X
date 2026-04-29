09c433f551e61821de1487acfa04e1898a17d51d
Damian <damian.marengo@cablasociados.com>
2026-04-29 19:28:26 -0300

    Cambios nomenclatura

--- tests/test_menu_principal.py
+++ tests/test_menu_principal.py
@@ -1,18 +1,18 @@
 from unittest.mock import patch
-import Menu_Principal
+import menu_principal
 
 
 # Testea que mostrar_menu devuelve lo ingresado
 def test_mostrar_menu():
     with patch("builtins.input", return_value="1"):
-        opcion = Menu_Principal.mostrar_menu()
+        opcion = menu_principal.mostrar_menu()
         assert opcion == "1"
 
 
 # Testea salir del programa
 def test_main_salir(capsys):
     with patch("builtins.input", side_effect=["5"]):
-        Menu_Principal.main()
+        menu_principal.main()
 
     captured = capsys.readouterr()
     assert "Saliendo del programa..." in captured.out
